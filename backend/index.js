d40809faaadbe5a09ac752081a949a0cc28160c5
Kamogelo <mmopanemoroka@gmail.com>
2025-02-25 17:48:46 +0200

    graphql setup

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,43 @@
+import express from "express";
+import http from "http";
+import cors from "cors";
 
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+
+import mergedResolvers from "./resolvers/index.js";
+import mergedTypeDef from "./typeDefs/index.js";
+
+const app = express();
+
+const httpServer = http.createServer(app);
+
+const server = new ApolloServer({
+  typeDefs: mergedTypeDef,
+  resolvers: mergedResolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+});
+
+// Ensure we wait for our server to start
+await server.start();
+
+// Set up our Express middleware to handle CORS, body parsing,
+// and our expressMiddleware function.
+app.use(
+  "/",
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  }),
+  express.json(),
+  // expressMiddleware accepts the same arguments:
+  // an Apollo Server instance and optional configuration options
+  expressMiddleware(server, {
+    context: async ({ req }) => ({ req }),
+  })
+);
+
+await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+
+console.log(`🚀 Server ready at http://localhost:4000/`);
