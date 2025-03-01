fa5188dc1369a63c176ca6c0c09398d74844aa0d
Kamogelo <mmopanemoroka@gmail.com>
2025-03-01 22:44:58 +0200

    routes & pages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import GridBackground from "./components/ui/GridBackgroun.jsx";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <GridBackground>
+        {/* <ApolloProvider client={client}> */}
+        <App />
+        {/* </ApolloProvider> */}
+      </GridBackground>
+    </BrowserRouter>
+  </StrictMode>
+);
